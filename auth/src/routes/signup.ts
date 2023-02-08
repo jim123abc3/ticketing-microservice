ba5426eb664bf1c9359e6c3855ce9bb35541614c
Jim Plimmer <jim123abc3@gmail.com>
2023-02-08 17:47:21 +0000

    add more routes and middlewares

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -15,13 +16,8 @@ router.post('/api/users/signup', [
     .isLength({ min: 4, max: 20 })
     .withMessage('Password must be between 4-20 chars')
 ],
+validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
@@ -32,6 +28,16 @@ router.post('/api/users/signup', [
 
     const user = User.build({ email, password });
     await user.save();
+    
+    // Generate JWT
+    const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+    }, process.env.JWT_KEY!);
+
+    req.session = {
+      jwt: userJwt
+    };
 
     res.status(201).send(user);
   });
