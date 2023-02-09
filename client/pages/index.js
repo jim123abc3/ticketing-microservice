ebd0b49c1d37588e59966ac80b494c44f62ceda0
Jim Plimmer <jim123abc3@gmail.com>
2023-02-09 16:59:26 +0000

    fixed signup page

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,25 +2,34 @@ import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
   // console.log(currentUser);
-  // axios.get('/api/users/currentuser').catch((err) => {
-  //   console.log(err.message);
-  // });
-
+  // axios.get('/api/users/currentuser');
   console.log(currentUser);
- 
+
   return <h1>Landing Page</h1>;
 };
 
 LandingPage.getInitialProps = async () => {
-  if(typeof window === 'undefined'){
+  if (typeof window === 'undefined') {
+    // we are on the server!
+    // requests should be made to http://ingress-nginx.ingress-nginx...laksdjfk
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+    );
 
-  }else {
+    return data;
+  } else {
+    // we are on the browser!
+    // requests can be made with a base url of ''
     const { data } = await axios.get('/api/users/currentuser');
 
     return data;
   }
-
-  return {}
+  return {};
 };
- 
+
 export default LandingPage;
