7fa81706ba841c463a0b6b7a8f7b3924d5fb4164
Jim Plimmer <jim123abc3@gmail.com>
2023-04-26 19:26:41 +0100

    add remaining client-side pages, integrate stripe FE

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,43 @@
-import buildClient from '../api/build-client';
+import Link from 'next/link';
 
-const LandingPage = ({ currentUser }) => {
-  return <h1>{`Signed ${currentUser ? 'in' : 'out'}`}</h1>
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map(ticket => {
+    return(
+      <tr key={ticket._id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link legacyBehavior href='/tickets/[ticketId]' as={`/tickets/${ticket._id}`}>
+            <a>View</a>
+          </Link>
+        </td>
+      </tr>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ticketList}
+        </tbody>
+      </table>
+    </div>
+  )
 };
 
-LandingPage.getInitialProps = async context => {
-  console.log('LANDING PAGE!!');
-  const client = buildClient(context);
-  const { data } = await client.get('/api/users/currentuser');
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  const {data} = await client.get('/api/tickets');
 
-  return data;
+  return { tickets: data };
 };
 
 export default LandingPage;
